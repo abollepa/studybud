8fb923a9aee82d0d5d1fad25e39ec42054312d9b
abollepa <abollepa@charlotte.edu>
2025-02-23 19:42:05 -0500

    Add files via upload

--- base/views.py
+++ base/views.py
@@ -1,22 +1,8 @@
 from django.shortcuts import render
-from .models import Room
-
-# Create your views here.
-
-rooms = [
-    {'id':1, 'name':'Lets learn python!'},
-    {'id':2, 'name':'Design with me'},
-    {'id':3, 'name':'Frontend developers'},
-]
-
-
+from django.http import HttpResponse
 
 def home(request):
-    rooms = Room.objects.all()
-    context = {'rooms':rooms}
-    return render(request, 'base/home.html', context)
+    return render(request,'home')
 
-def room(request, pk):
-    room = Room.objects.get(id=pk)
-    context = {'room':room}
-    return render(request, 'base/room.html',context)
+def room(request):
+    return render(request,'room')
